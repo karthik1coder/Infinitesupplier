345e3b35153380456ea755491306fe857508eea3
karthik1coder <karthik1coder@gmail.com>
2025-08-23 18:25:35 +0530

    removing unnecessary directory

--- Downloads/InfiniteSupplier/src/main/java/com/instantbusiness/repository/locationRepo.java
+++
@@ -1,8 +0,0 @@
-package com.instantbusiness.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.instantbusiness.Entity.location;
-public interface locationRepo  extends JpaRepository<location, Long> {
-    
-}
